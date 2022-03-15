9f793063bd7ff9859e7da115db60f28c2ec8270c
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-15 16:54:54 -0400

    Changing the baseurl to twitter-clone-api Heroku app. Should be able to retrieve data on this Heroku from that one.

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+//For testing:
+// export default axios.create({
+//     baseURL: "http://localhost:5000/api/v1/tweets",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
+
+//For production:
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/tweets",
+    baseURL: "https://dry-citadel-53300.herokuapp.com/api/v1/tweets",
     headers: {
         "Content-type": "application/json"
     }
